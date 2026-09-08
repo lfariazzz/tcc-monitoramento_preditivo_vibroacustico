fca2726f856e011950a26969ab46baba8354c1e4
lfariazzz <levi.farias@aluno.ufca.edu.br>
2026-09-08 20:30:40 -0300

    chore(build): versiona sdkconfig e arquivos complementares para virar repositorio IDF

---
+++ firmware/main/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void app_main(void)
+{
+    printf("Firmware inicializado.\n");
+    // TODO: criar semáforo, mutex_i2c, mutex_sd, fila_eventos_rede
+    // TODO: xTaskCreate das 3 tasks
+}
